211a23d65c6e4feaf932ab75f9366999d50df5b6
tgry <26769109+tgry@users.noreply.github.com>
2020-10-03 23:20:06 +0900

    PWA設定ありでビルド

---
+++ docs/precache-manifest.62b5929edf5b3c1fee0c169013ed584b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f093afbd8ddf7aeaa14",
+    "url": "/glossary-tool/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "09b9851be8d07867014c",
+    "url": "/glossary-tool/css/chunk-vendors.6a7c8892.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/glossary-tool/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "acce3405e85c694735903936a1346c05",
+    "url": "/glossary-tool/index.html"
+  },
+  {
+    "revision": "8f093afbd8ddf7aeaa14",
+    "url": "/glossary-tool/js/app.1915c60e.js"
+  },
+  {
+    "revision": "09b9851be8d07867014c",
+    "url": "/glossary-tool/js/chunk-vendors.c03128b3.js"
+  },
+  {
+    "revision": "b8f0b326ea946e58446dc9de0edcac0b",
+    "url": "/glossary-tool/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/glossary-tool/robots.txt"
+  }
+]);
